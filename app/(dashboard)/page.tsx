d613f9fd837561ce2e5a957b304c5122000440d1
Sanaz <s.shahraeini@gmail.com>
2024-05-26 01:23:25 +0330

    create structure of auth

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+
+const page = () => {
+  return (
+    <div>
+      <Button variant="destructive">Click me</Button>
+    </div>
+  )
+}
+
+export default page
